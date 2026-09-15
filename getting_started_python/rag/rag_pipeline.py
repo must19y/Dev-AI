39f4c76b9d728fcbcd300a974fdfac65c3464d67
must19y <128187934+must19y@users.noreply.github.com>
2026-09-15 11:24:20 +0000

    Rag for  document query

--- getting_started_python/rag/rag_pipeline.py
+++ getting_started_python/rag/rag_pipeline.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 from ollama import Client
 
 from document_vector_store import DocumentVectorStore
+from FastAPI import fastapi
 
 document = Path("sample_code").read_text()
 
@@ -45,4 +46,119 @@ for chunk in response:
     if response.message.content:
         print(response.message.content,end="",flush=True)
 
-print()
+##print()
+
+
+app = FastAPI()
+
+
+@app.post("/")
+def handle_post(chat_request:ChatRequest):
+    question = chat_request.question
+
+    conversation_history = Conversation_history({
+        role: "system",
+        content: system_message.render()
+    })
+
+
+    for msg in chat_request.history:
+        conversation_history.add_message({
+            role:msg.role,
+            content:msg.content
+        })
+
+
+    documents = vector_store.query(question)
+
+    conversation_history.add_message({
+        role:"user",
+        content:user_message.render(documents=documents,question=question)
+    })
+
+    return StreamingResponse(generate_stream(conversation_history))
+    
+
+
+
+
+
+
+    ##api service for multidocument
+
+    from FastAPI import fastapi
+    from document_vector_store import MultiDocumentVectorStore
+    from conversation_history import ConversationHistory
+    from main import ChatRequest
+
+
+    client = Client()
+
+    env = Environment(
+        FileSystemLoader(
+
+        )
+    )
+
+    readme_filenameas=[
+
+    ]
+
+    readme_documents:List[Documents] = []
+
+    for readme_filename in readme_filenames:
+        readme_documents.append(
+            download_remote_document(filename=readme_filename)
+        )
+
+    document= MultiDocumentVectorStore(readme_documents)
+
+    system_message = env.get_template("basic_support_system")
+    user_message=env.get_template("basic_support_user")
+    
+
+    response = client.chat(
+        model="llama-3.2b",
+        messages= messages,
+        stream= True,
+        options={
+            temperature: 0
+        }
+    )
+
+    for chunk in response:
+        if chunk.message.content:
+
+            ##snip 
+
+## till here is the way we printwithoutha service jsut that
+app = FastAPI()
+
+@app.post("/")
+def user_query(chat:ChatRequest)
+    conversation_history= Conversation_history({
+        role:"system",
+        content:system_message.render()
+    })
+
+    for message in chat:
+        conversation_history.add_message({
+            role: message.role,
+            content: message.content
+        })
+
+    documents = document.query(question,3)
+
+    conversation_history.add_message({
+        role: "user",
+        content: user_message.render(
+            documents=documents,
+            question= question
+        )
+    })
+    
+    return StreamingResponse(
+        generate_stream(conversation_history)
+    )
+
+     
